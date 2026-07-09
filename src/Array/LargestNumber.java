13dcb503dc7ee4fe6b99017f395caad68c89cf4d
codewithria89 <anjalik0822@gmail.com>
2026-07-09 22:55:46 +0530

    Adding new files

---
+++ src/Array/LargestNumber.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i]  = sc.nextInt();
+        }
+        int max = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Maximum Array: " + max);
+    }
+}
