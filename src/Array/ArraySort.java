43d655fee25fbf495b057e1e774c53dde65e2cd0
codewithria89 <anjalik0822@gmail.com>
2026-07-10 11:36:26 +0530

    Checking array is sorted or not

---
+++ src/Array/ArraySort.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < n-1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                isSorted = false;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        if(isSorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
+}
